3239c6b03655d573022a25a8e734bb97bb3d48d0
AlejandroCedeno06 <cedenoalejandro0612@gmail.com>
2026-02-20 23:29:50 -0600

    Agregamos un centro de seguridad

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
-const password = "seguridad2026";
+const PASSWORD = "ElprofeOcampoesGood";
 
-let acceso = prompt("Ingrese contraseña para ver la documentación:");
+function verificarPassword(){
 
-if(acceso !== password){
-document.body.innerHTML =
-"<h1 style='text-align:center;margin-top:20%'>❌ Acceso Denegado</h1>";
+    const input = document.getElementById("passwordInput").value;
+    const error = document.getElementById("errorMsg");
+
+    if(input === PASSWORD){
+        document.getElementById("loginOverlay").style.display = "none";
+    }else{
+        error.textContent = "❌ Contraseña incorrecta";
+    }
 }
+
+/* ENTER = ingresar */
+document.addEventListener("keypress", function(e){
+    if(e.key === "Enter"){
+        verificarPassword();
+    }
+});
